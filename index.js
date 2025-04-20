7914fe1b68a359961d9f463f3087be4647ffbf86
DavidTrindade <dave.ferrari579@gmail.com>
2025-04-21 00:20:08 +0100

    feat: Add daily endpoint, contains all daily puzzles

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/puzzles', async (req, res) => {
+app.get('/puzzles', async (_req, res) => {
     const { data, error } = await supabase
         .from("puzzle_definitions")
         .select("*")
         .order("created_at")
+
+    if (error) {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
+
+    return res.json(data);
+})
+
+app.get('/daily', async (_req, res) => {
+    const { data, error } = await supabase
+        .from("daily_puzzle")
+        .select("*")
+        .order("date")
 
     if (error) {
         console.error(error);
